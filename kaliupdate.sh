650233d2af900091170f9339068c1b948cfc9533
Eric Humphries <eric@ifyd.com>
2017-11-27 14:55:29 -0600

    quick update
    quick update. removed duplicate package and break lines so they wrap

--- kaliupdate.sh
+++ kaliupdate.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 echo "updating kali"
-apt-get install bluez bluez-test-scripts bluez-test-scripts python-bluez python-dbus ubertooth sqlite3 htop nmap nfs-common libsqlite3-dev open-vm-tools-desktop fuse linux-headers-amd64 realtek-rtl88xxau-dkms metasploit-framework htop
+apt-get install bluez bluez-test-scripts bluez-test-scripts python-bluez python-dbus ubertooth sqlite3 \
+htop nmap nfs-common libsqlite3-dev open-vm-tools-desktop fuse linux-headers-amd64 realtek-rtl88xxau-dkms \
+metasploit-framework
 apt-get -y update && apt-get -y upgrade && apt autoremove && apt-get -y dist-ugprade
