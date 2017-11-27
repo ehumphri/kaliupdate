e2c15b9b33093917bea80cb000377d036f9b2182
Eric Humphries <eric@ifyd.com>
2017-11-27 15:08:45 -0600

    adding a couple things
    added some necessary tools

--- kaliupdate.sh
+++ kaliupdate.sh
@@ -5,3 +5,8 @@ apt-get install bluez bluez-test-scripts bluez-test-scripts python-bluez python-
 htop nmap nfs-common libsqlite3-dev open-vm-tools-desktop fuse linux-headers-amd64 realtek-rtl88xxau-dkms \
 metasploit-framework
 apt-get -y update && apt-get -y upgrade && apt autoremove && apt-get -y dist-ugprade
+
+# install additional shit
+git clone https://github.com/anexia-it/winshock-test.git 
+git clone https://github.com/portcullislabs/rdp-sec-check
+git clone https://github.com/ChrisTruncer/EyeWitness.git
