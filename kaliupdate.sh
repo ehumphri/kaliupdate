5bbe341aa45cac15e5b0c080e983e72313989196
Eric Humphries <eric@ifyd.com>
2017-11-27 15:09:49 -0600

    spleelllinnnggg
    i can't type

--- kaliupdate.sh
+++ kaliupdate.sh
@@ -4,7 +4,7 @@ echo "updating kali"
 apt-get install bluez bluez-test-scripts bluez-test-scripts python-bluez python-dbus ubertooth sqlite3 \
 htop nmap nfs-common libsqlite3-dev open-vm-tools-desktop fuse linux-headers-amd64 realtek-rtl88xxau-dkms \
 metasploit-framework
-apt-get -y update && apt-get -y upgrade && apt autoremove && apt-get -y dist-ugprade
+apt-get -y update && apt-get -y upgrade && apt autoremove && apt-get -y dist-upgrade
 
 # install additional shit
 git clone https://github.com/anexia-it/winshock-test.git 
