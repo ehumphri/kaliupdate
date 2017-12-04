c708fe995ab083092560d61957eacb16a9c23bc3
Eric Humphries <eric@ifyd.com>
2017-12-04 12:59:36 -0600

    add nerd deps
    add nerd deps

--- kaliupdate.sh
+++ kaliupdate.sh
@@ -3,7 +3,7 @@
 echo "updating kali"
 apt-get install bluez bluez-test-scripts bluez-test-scripts python-bluez python-dbus ubertooth sqlite3 \
 htop nmap nfs-common libsqlite3-dev open-vm-tools-desktop fuse linux-headers-amd64 realtek-rtl88xxau-dkms \
-metasploit-framework
+metasploit-framework libssl-dev libreadline-dev
 apt-get -y update && apt-get -y upgrade && apt autoremove && apt-get -y dist-upgrade
 
 ## install additional shit
